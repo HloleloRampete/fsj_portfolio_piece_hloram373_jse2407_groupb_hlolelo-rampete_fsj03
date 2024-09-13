11a269434a1a242a5d9aa3e8ac3ab713198b1e58
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-09-13 10:20:52 +0200

    "Added previous and next button to navigate to currentPage -1 or +1"

--- components/Pagination.js
+++ components/Pagination.js
@@ -10,4 +10,25 @@ export default function Pagination({ currentPage, totalProducts, productsPerPage
         window.location.search = params.toString()
     }
 
+    return (
+        <div className="flex justify-center items-center space-x-4 mt-8">
+      <button
+        onClick={() => handlePageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+        className="px-4 py-2 bg-blue-500 text-white rounded disabled:bg-gray-300"
+      >
+        Previous
+      </button>
+      <span>
+        Page {currentPage}
+      </span>
+      <button
+        onClick={() => handlePageChange(currentPage + 1)}
+        // disabled={currentPage}
+        className="px-4 py-2 bg-blue-500 text-white rounded disabled:bg-gray-300"
+      >
+        Next
+      </button>
+    </div>
+    )
 }
